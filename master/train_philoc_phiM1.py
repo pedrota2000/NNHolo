e1595a5052c190e91b1e33a4c9f393501e4c56b8
Pedro <79938018+pedrota2000@users.noreply.github.com>
2024-03-06 15:50:33 +0100

    Update train_philoc_phiM1.py

--- master/train_philoc_phiM1.py
+++ master/train_philoc_phiM1.py
@@ -14,7 +14,7 @@ from platform import python_version
 class train_model():
     def __init__(self,data_path,results_path = "train_results",sampling = "chebyshev2"):
         import numpy
-        from NNholo import *
+        import NNHolo
         from tqdm.auto import tqdm
         # Directory
         directory = results_path
